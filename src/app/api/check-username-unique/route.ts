b03693580d77d947910cfe94d7f6c4c19070ca6e
barkat <sekhbarkatkudrathofficial@gmail.com>
2024-07-12 01:42:28 +0530

    api for verify user and unique or not

---
+++ src/app/api/check-username-unique/route.ts
@@ -0,0 +1,57 @@
+import { dbConnect } from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import {z} from "zod"
+import { usernameValidation } from "@/Schemas/signUpSchema";
+
+
+const UsernameQuerySchema = z.object({
+    username : usernameValidation
+})
+
+export async function GET(request:Request){
+  
+    await dbConnect()
+    try{
+        const {searchParams} = new URL(request.url)
+        const queryParams = {
+            username:searchParams.get("username")
+        }
+        //validate with zod
+       const result = UsernameQuerySchema.safeParse(queryParams)
+       if(!result.success){
+        const usernameErrors = result.error.format().username?._errors || []
+        return Response.json({
+            success:false,
+            message:usernameErrors?.length > 0 ? usernameErrors.join(', ') : 'Invalid query parameter',
+        },
+        {
+            status:400
+        }
+    )
+       }
+
+       const  {username} = result.data
+       const existingVerifiedUser = await UserModel.findOne({username,isVerified:true})
+       if(existingVerifiedUser){
+        return Response.json({
+            success:true,
+            message:"Username is Unique"
+        },
+        {
+            status:400
+        }
+    )
+       }
+
+    }catch(error){
+        console.log("Error checking username",error)
+        return Response.json(
+            {
+                success:false,
+                message:"Error checking username"
+            },{
+                status:500
+            }
+        )
+    }
+}
